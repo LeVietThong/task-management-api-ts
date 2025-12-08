798034004032735cc5bd06329a4384f1d0f8723a
ThongLV <levietthong92@gmail.com>
2025-12-08 10:34:12 +0700

    phân trang

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import Task from "../models/task.model";
+import paginationHelper from "../../../helpers/pagination";
 
 export const index = async (req: Request, res: Response) => {
   //Find
@@ -15,17 +16,35 @@ export const index = async (req: Request, res: Response) => {
   if (req.query.status) {
     find.status = req.query.status.toString();
   }
+  //End Find
+
+  //Pagination
+  let initPagination = {
+    currentPage: 1,
+    limitItems: 2,
+  };
+
+  const countTasks = await Task.countDocuments(find);
+  const objectPagination = paginationHelper(
+    initPagination,
+    req.query,
+    countTasks
+  );
+  //End Pagination
 
   //Sort
-  const sort = {
-  };
+  const sort = {};
 
   if (req.query.sortKey && req.query.sortValue) {
     const sortKey = req.query.sortKey.toString();
     sort[sortKey] = req.query.sortValue;
   }
+  //End Sort
 
-  const tasks = await Task.find(find);
+  const tasks = await Task.find(find)
+    .sort(sort)
+    .limit(objectPagination.limitItems)
+    .skip(objectPagination.skip);
 
   res.json(tasks);
 };
